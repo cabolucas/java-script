16f4380b617d04572c032c4a0c8d02301aa5661b
cabolucas <lucascabo@outlook.com>
2025-10-10 09:18:23 -0300

    tabuada

---
+++ aula01/ex017/ex017.js
@@ -0,0 +1,12 @@
+function tabuada() {
+let num = window.document.getElementById('txtn')
+let tab = window.document.getElementById('seltab')
+
+if (num.value.length == 0) {
+    window.alert('por favor digite um número')
+    
+} else {
+    let num = Number(num.value)
+}
+
+}
