573a800d52b608b25fd0fe0daf684b184aff08ed
cabolucas <lucascabo@outlook.com>
2025-10-10 19:30:43 -0300

    atualização

--- aula01/ex017/ex017.js
+++ aula01/ex017/ex017.js
@@ -3,10 +3,17 @@ let num = window.document.getElementById('txtn')
 let tab = window.document.getElementById('seltab')
 
 if (num.value.length == 0) {
-    window.alert('por favor digite um número')
-    
+    window.alert('por favor digite um número')    
 } else {
-    let num = Number(num.value)
+    let n = Number(num.value)
+    let c = 1
+    tab.innerHTML = ''
+    while (c <= 10) {
+        let item = document.createElement('option')
+        item.text = `${n} x ${c} = ${n*c}`
+        tab.appendChild(item)
+        c++
+    }
 }
 
 }
