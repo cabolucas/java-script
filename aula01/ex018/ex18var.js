d612018083348658b7c1c23d00163c8b7727859f
cabolucas <lucascabo@outlook.com>
2025-10-10 20:11:05 -0300

    ex018

---
+++ aula01/ex018/ex18var.js
@@ -0,0 +1,10 @@
+let num = [5, 8 ,2, 9, 3]
+
+num[3] = 6 // para acrescentar dentro da variavel escolhendo o local
+num.push(7) // já acrescenta o espaço sem eu precisar escolher o espaço
+num.length // é o numero de caracteres que tem na variável
+
+num.sort() //organiza os numeros na variável em ordem crescente
+
+
+console.log(`nosso vetor é o ${num}`)
