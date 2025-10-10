086d7dd1cb54e52f91877d05093f1666a7753839
cabolucas <lucascabo@outlook.com>
2025-10-10 00:29:06 -0300

    organizando

---
+++ aula01/ex015/ex015.js
@@ -0,0 +1,36 @@
+
+
+// 1ª MODELO TESTA E DEPOIS SEGUE 
+/* let c = 1
+while (c <= 10) {
+    console.log('olá tudo bem?')
+    c++ // soma + 1 a variável
+}
+*/
+
+// 2ª MODELO EXECUTA E DEPOIS TESTA
+/*
+let c = 1
+do {
+    console.log('ola tudo bem ?')
+    c++
+} while(c <= 10)
+*/
+
+// 3ª MODELO inicialização, teste, incremento
+
+console.log('vai começar...')
+for (let c = 1;c <= 5;c++) {
+    console.log(c)
+} 
+console.log('fim')
+
+
+/*
+console.log('tudo bem ?')
+console.log('tudo bem ?')
+console.log('tudo bem ?')
+console.log('tudo bem ?')
+console.log('tudo bem ?')
+console.log('tudo bem ?')
+*/
