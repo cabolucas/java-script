b5e6e6bc8b0acaca75f53c2c00bc038a19476d25
cabolucas <lucascabo@outlook.com>
2025-10-10 08:38:50 -0300

    Update ex016.js

--- aula01/ex016/ex016.js
+++ aula01/ex016/ex016.js
@@ -8,14 +8,20 @@ let res = window.document.getElementById('res')
 if (ini.value.length == 0 || nfim.value.length == 0 || npas.value.length == 0) {
     window.alert('Verifique os dados inseridos')
 } else {
-    res.innerHTML = 'Contando:'
+    res.innerHTML = 'Contando: '
     let i = Number(ini.value)
     let f = Number(nfim.value)
     let p = Number(npas.value)
-    
-    for(c = i; c <= f; c += p) {
+    if (i < f) {
+        for(c = i; c <= f; c += p) {
         res.innerHTML += `${c}`
     }
+    } else {
+        for(let c = i; c >= f; c -+ p) {
+            res.innerHTML += `${c}`
+        }
+    }
+    
 
 }
 
