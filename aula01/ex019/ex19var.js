775eaa5777e364850d30b04f281737d8114619da
cabolucas <lucascabo@outlook.com>
2025-10-11 23:30:31 -0300

    Update ex19var.js

--- aula01/ex019/ex19var.js
+++ aula01/ex019/ex19var.js
@@ -1,7 +1,4 @@
 /*
-
-*/
-
 function parimpar(n) { 
     if (n%2 == 0) {
         return 'Par'
@@ -10,8 +7,12 @@ function parimpar(n) {
     }
 }
 
-let res = parimpar(4) // chamada
+let res = parimpar(4) /*chamada
 
 console.log(res)
+*/
 
-
+function soma(n1=0, n2=0) {  // o = 0 é para quando não for dado o valor de n1 ou n2 o valor irá aparecer zero
+    return n1 + n2
+}
+console.log(soma(2, 5)) // dando o valor de n1 e n2
